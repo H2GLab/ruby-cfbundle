45368b8c135bb4014cec8b49aee1a04535151c0f
Nicolas Bachschmidt <nicolas@h2g.io>
2018-12-04 16:09:00 +0100

    Bump version number to 0.2.2

--- spec/models/cfbundle_spec.rb
+++ spec/models/cfbundle_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 require 'cfbundle'
 
 RSpec.describe CFBundle do
-  it 'is version 0.2.1' do
-    expect(subject::VERSION).to eq('0.2.1')
+  it 'is version 0.2.2' do
+    expect(subject::VERSION).to eq('0.2.2')
   end
 end
